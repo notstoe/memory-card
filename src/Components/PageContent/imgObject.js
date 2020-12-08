e94a062c874f7201d1c08aa9e06342116d6afee6
notstoe <gstonin95@gmail.com>
2020-12-08 19:07:13 +0000

    initial commit assets obj

---
+++ src/Components/PageContent/imgObject.js
@@ -0,0 +1,80 @@
+import apple from "./assets/apple.jpg";
+import banana from "./assets/banana.jpg";
+import clementine from "./assets/clementine.jpg";
+import grapes from "./assets/grapes.jpg";
+import guava from "./assets/guava.jpg";
+import kiwi from "./assets/kiwi.jpg";
+import lemon from "./assets/lemon.jpg";
+import mango from "./assets/mango.jpeg";
+import passionfruit from "./assets/passionfruit.jpg";
+import peach from "./assets/peach.jpg";
+import pear from "./assets/pear.jpg";
+import pitaya from "./assets/pitaya.jpg";
+import strawberry from "./assets/strawberry.jpg";
+import tomato from "./assets/tomato.jpg";
+import watermelon from "./assets/watermelon.jpg";
+
+const imgs = [
+	{
+		image: apple,
+		alt: "apple",
+	},
+	{
+		image: banana,
+		alt: "banana",
+	},
+	{
+		image: clementine,
+		alt: "clementine",
+	},
+	{
+		image: grapes,
+		alt: "grapes",
+	},
+	{
+		image: guava,
+		alt: "guava",
+	},
+	{
+		image: kiwi,
+		alt: "kiwi",
+	},
+	{
+		image: lemon,
+		alt: "lemon",
+	},
+	{
+		image: mango,
+		alt: "mango",
+	},
+	{
+		image: passionfruit,
+		alt: "passionfruit",
+	},
+	{
+		image: peach,
+		alt: "peach",
+	},
+	{
+		image: pear,
+		alt: "pear",
+	},
+	{
+		image: pitaya,
+		alt: "pitaya",
+	},
+	{
+		image: strawberry,
+		alt: "strawberry",
+	},
+	{
+		image: tomato,
+		alt: "tomato",
+	},
+	{
+		image: watermelon,
+		alt: "watermelon",
+	},
+];
+
+export default imgs;
